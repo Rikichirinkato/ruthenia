a2f16f97eeb9944d089e7e313910b602bc7eb463
karljohan <roman28krasko@gmail.com>
2018-05-02 18:47:45 +0300

    add all cities and kings to json, search error

--- src/app/citiespage/citiespage.component.ts
+++ src/app/citiespage/citiespage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material';
 import {CityItemComponent} from './city-item/city-item.component';
 
@@ -7,64 +7,24 @@ import {CityItemComponent} from './city-item/city-item.component';
   templateUrl: './citiespage.component.html',
   styleUrls: ['./citiespage.component.css']
 })
-export class CitiespageComponent {
+export class CitiespageComponent implements OnInit {
 
   objectKeys = Object.keys;
 
   constructor(public dialog: MatDialog) { }
 
-  public citiesList = {
-    Galicia: {
-      Halych: {
-        name: 'Halych',
-        found: 9
-      },
-      Lviv: {
-        name: 'Lviv',
-        found: 13
-      },
-      Peremyshyl: {
-        name: 'Peremyshyl',
-        found: 9
-      },
-    },
-    Volyn: {
-      Volodymyr: {
-        name: 'Volodymyr',
-        found: 10
-      },
-      Lutsk: {
-        name: 'Lutsk',
-        found: 11
-      }
-    },
-    Kyiv: {
-      Kyiv: {
-        name: 'Kyiv',
-        found: 5
-      },
-      Istkorosten: {
-        name: 'Istkorosten',
-        found: 8
-      }
-    },
-    Turov: {
-      Turov: {
-        name: 'Turov',
-        found: 10
-      },
-      Pinsk: {
-        name: 'Pinsk',
-        found: 8
-      }
-    }
-  };
+  public cities: Object;
+  ngOnInit () {
+    fetch('../assets/data.json')
+      .then( response => response.json())
+      .then( response => this.cities = Object.assign({}, response.citiesList));
+  }
 
   openCity(town, key): void {
     const dialogRef = this.dialog.open(CityItemComponent, {
       width: '400px',
       height: '400px',
-      data: { city: town, key: key, info: this.citiesList }
+      data: { city: town, key: key, info: this.cities }
     });
   }
 
